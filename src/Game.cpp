d9e00c966fbd4761ca6b25d72d9d9135b5938d06
Squid <luis.bazan@unity3d.com>
2024-12-18 23:41:08 -0500

    Delta Time added

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,5 @@
 #include "Game.h"
-#include <iostream>
+
 
 Game::Game(){
     std::cout << "Game constructor called!"<< std::endl;
@@ -28,7 +28,7 @@ void Game::Initialize(){
         SDL_WINDOWPOS_CENTERED, 
         windowWidth,
         windowHeight,
-        SDL_WINDOW_FULLSCREEN
+        SDL_WINDOW_BORDERLESS
     );
 
     if(!window){
@@ -36,7 +36,7 @@ void Game::Initialize(){
         return;
     }
 
-    renderer = SDL_CreateRenderer(window, -1, 0, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if(!renderer){
         std::cerr << "Error creating SDL2 renderer" << std::endl;
         return;
@@ -44,13 +44,24 @@ void Game::Initialize(){
 
     isRunning = true;
 }
+
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
+void Game::Setup(){
+    playerPosition = glm::vec2(10.0, 20.0);
+    playerVelocity = glm::vec2(10.0, 0.0);
+}
+
 void Game::Run(){
+    Setup();
     while(isRunning){
         ProcessInput();
         Update();
         Render();
     }
 }
+
 void Game::ProcessInput(){
     SDL_Event sdlEvent;
     while(SDL_PollEvent(&sdlEvent)){
@@ -68,12 +79,40 @@ void Game::ProcessInput(){
 }
 
 void Game::Update(){
+    //while(!SDL_TICKS_PASSED(SDL_GetTicks(), millisecondsPreviousFrame + MILLISECS_PER_FRAME));
+    int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecondsPreviousFrame);
+    if(timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME){
+        SDL_Delay(timeToWait);
+    }
 
+    double deltaTime = (SDL_GetTicks() - millisecondsPreviousFrame) / 1000.0f;
+
+    millisecondsPreviousFrame = SDL_GetTicks();
+
+    playerPosition.x += playerVelocity.x * deltaTime;
+    playerPosition.y += playerVelocity.y * deltaTime;
 }
 
 void Game::Render(){
-    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
     SDL_RenderClear(renderer);
+
+    SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    SDL_Rect destRect = {
+                         static_cast<int>(playerPosition.x), 
+                         static_cast<int>(playerPosition.y), 
+                         32, 
+                         32};
+
+    SDL_RenderCopy(renderer, texture, NULL, &destRect);
+    SDL_DestroyTexture(texture);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_Rect player = {10, 10, 20, 20};
+    SDL_RenderFillRect(renderer, &player);
 
     SDL_RenderPresent(renderer);
 }
--- src/Game.h
+++ src/Game.h
@@ -1,18 +1,28 @@
 #ifndef GAME_H
 #define GAME_H
+
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <iostream>
+#include <glm/glm.hpp>
+
+const int FPS = 120;
+const int MILLISECS_PER_FRAME = 1000 / FPS;
 
 class Game{
     private:
         bool isRunning;
         SDL_Window* window;
         SDL_Renderer* renderer;
+        int millisecondsPreviousFrame = 0;
 
     public:
         Game();
         ~Game();
         void Initialize();
         void Run();
+        void Setup();
         void ProcessInput();
         void Update();
         void Render();
