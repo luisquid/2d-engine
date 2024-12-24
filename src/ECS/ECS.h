111f8631bc870fde82ca7ef48073830034b4b8d6
Squid <luis.bazan@unity3d.com>
2024-12-23 21:39:18 -0500

    ECS Structure created

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -1,20 +1,63 @@
 #ifndef ECS_H
 #define ECS_H
 
-class Component{
+#include <bitset>
+#include <vector>
 
+const unsigned int MAX_COMPONENTS = 32;
+
+typedef std::bitset<MAX_COMPONENTS> Signature;
+
+struct IComponent{
+    protected:
+        static int nextId;
+};
+
+template <typename TComponent>
+class Component: public IComponent{
+    static int GetId(){
+        static auto id = nextId++;
+        return id;
+    }
 };
 
 class Entity{
+    private:
+        int id;
 
+    public:
+        Entity(int _id) : id(_id){};
+        int GetId() const;
+
+        Entity& operator =(const Entity& other) = default;
+        bool operator ==(const Entity& otherEntity) const{ return id == otherEntity.id; }
+        bool operator !=(const Entity& otherEntity) const{ return id != otherEntity.id; }
 };
 
 class System{
+    private:
+        Signature componentSignature;
+        std::vector<Entity> entities;
 
+    public:
+        System() = default;
+        ~System() = default;
+
+        void AddEntityToSystem(Entity entity);
+        void RemoveEntityFromSystem(Entity entity);
+        std::vector<Entity> GetSystemEntities() const;
+        const Signature& GetComponentSignature() const;
+
+        template <typename TComponent> void RequireComponent();
 };
 
 class Registry{
 
 };
 
+template <typename TComponent>
+void System::RequireComponent(){
+    const auto componentId = Component<TComponent>::GetId();
+    componentSignature.set(componentId);
+}
 #endif
--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -48,8 +48,7 @@ glm::vec2 playerPosition;
 glm::vec2 playerVelocity;
 
 void Game::Setup(){
-    playerPosition = glm::vec2(10.0, 20.0);
-    playerVelocity = glm::vec2(10.0, 0.0);
+    
 }
 
 void Game::Run(){
@@ -87,31 +86,11 @@ void Game::Update(){
     double deltaTime = (SDL_GetTicks() - millisecondsPreviousFrame) / 1000.0f;
 
     millisecondsPreviousFrame = SDL_GetTicks();
-
-    playerPosition.x += playerVelocity.x * deltaTime;
-    playerPosition.y += playerVelocity.y * deltaTime;
 }
 
 void Game::Render(){
     SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
     SDL_RenderClear(renderer);
-
-    SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-
-    SDL_Rect destRect = {
-                         static_cast<int>(playerPosition.x), 
-                         static_cast<int>(playerPosition.y), 
-                         32, 
-                         32};
-
-    SDL_RenderCopy(renderer, texture, NULL, &destRect);
-    SDL_DestroyTexture(texture);
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_Rect player = {10, 10, 20, 20};
-    SDL_RenderFillRect(renderer, &player);
 
     SDL_RenderPresent(renderer);
 }
